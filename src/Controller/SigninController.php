9c6ac64c3f036336c74f4f3242c5af39c7fe8c5f
amine-cheraitia <amine-cheraitia@hotmail.com>
2024-07-06 01:57:00 +0200

    add saving user

--- src/Controller/SigninController.php
+++ src/Controller/SigninController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\SigninType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SigninController extends AbstractController
 {
     #[Route('/signin', name: 'app_signin')]
-    public function index(): Response
+    public function index(Request $req, EntityManagerInterface $entityManager): Response
     {
+        $user = new User();
+        $form = $this->createForm(SigninType::class, $user);
+        $form->handleRequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
         return $this->render('signin/index.html.twig', [
-            'controller_name' => 'SigninController',
+            'form' => $form->createView(),
         ]);
     }
 }
