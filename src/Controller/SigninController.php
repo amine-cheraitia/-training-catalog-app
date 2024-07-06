5f273f477a695c7218665bf1762532434f820f24
amine-cheraitia <amine-cheraitia@hotmail.com>
2024-07-06 17:22:44 +0200

    add hash to password

--- src/Controller/SigninController.php
+++ src/Controller/SigninController.php
@@ -9,17 +9,23 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class SigninController extends AbstractController
 {
     #[Route('/signin', name: 'app_signin')]
-    public function index(Request $req, EntityManagerInterface $entityManager): Response
+    public function index(Request $req, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(SigninType::class, $user);
         $form->handleRequest($req);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
+            $plaintextPassword = $user->getPassword();
+            $hashedPassword = $passwordHasher->hashPassword($user, $plaintextPassword);
+            $user->setPassword($hashedPassword);
+
             $entityManager->persist($user);
             $entityManager->flush();
         }
